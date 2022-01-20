7e81aa4d047b6d40fe51d8cd8d719e869a635f87
Stefan Ober <stefan.ober@shitnet.de>
2022-01-20 13:55:03 +0100

    Skript für OU Erstellung

---
+++ Skripte/New-OuStructure.ps1
@@ -0,0 +1,13 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$Abteilungsname
+)
+
+New-ADOrganizationalUnit -Name:"$Abteilungsname" -Path:"OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+Set-ADObject -Identity:"OU=$Abteilungsname,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+New-ADOrganizationalUnit -Name:"Computer" -Path:"OU=$Abteilungsname,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+Set-ADObject -Identity:"OU=Computer,OU=$Abteilungsname,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+New-ADOrganizationalUnit -Name:"User" -Path:"OU=$Abteilungsname,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+Set-ADObject -Identity:"OU=User,OU=$Abteilungsname,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"GL-$Abteilungsname" -Path:"OU=$Abteilungsname,OU=Struktur,DC=ppedv,DC=test" -SamAccountName:"GL-$Abteilungsname" -Server:"Server1.ppedv.test"
