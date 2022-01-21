29ff874eb723603ad516dafd90ceedeac6025851
Stefan Ober <stefan.ober@shitnet.de>
2022-01-21 10:05:03 +0100

    Skript für ShareGroups

---
+++ Skripte/New-ShareGroups.ps1
@@ -0,0 +1,17 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$GruppenName,
+
+    [string]$LDAPDestinationpath = "OU=Gruppen,OU=Infrastruktur,OU=Struktur,DC=ppedv,DC=test" 
+    )
+
+foreach($Name in $GruppenName)
+{
+New-ADGroup -GroupCategory:"Security" -GroupScope:"DomainLocal" -Name:"DL-$GruppenName-R" -Path:"$LDAPDestinationpath" -SamAccountName:"DL-$GruppenName-R" -Server:"Server1.ppedv.test"
+New-ADGroup -GroupCategory:"Security" -GroupScope:"DomainLocal" -Name:"DL-$GruppenName-W" -Path:"$LDAPDestinationpath" -SamAccountName:"DL-$GruppenName-W" -Server:"Server1.ppedv.test"
+Add-ADPrincipalGroupMembership -Identity:"CN=DL-$GruppenName-W,$LDAPDestinationpath" -MemberOf:"CN=DL-$GruppenName-R,$LDAPDestinationpath" -Server:"Server1.ppedv.test"
+New-ADGroup -GroupCategory:"Security" -GroupScope:"DomainLocal" -Name:"DL-$GruppenName-F" -Path:"$LDAPDestinationpath" -SamAccountName:"DL-$GruppenName-F" -Server:"Server1.ppedv.test"
+Add-ADPrincipalGroupMembership -Identity:"CN=DL-$GruppenName-F,$LDAPDestinationpath" -MemberOf:"CN=DL-$GruppenName-W,$LDAPDestinationpath" -Server:"Server1.ppedv.test"
+}
+
